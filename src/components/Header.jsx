4c4128a14e01930760cb3ba6da76353d8fea765c
Yaroslav <the.khambir@gmail.com>
2024-05-28 21:24:54 +0200

    components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+ import '../components/Header.css'
+ export default function HeaderMod (){
+    return(<header className="header-block">
+    <h2>Git-test</h2>
+    <div className="wrap-menu">
+        <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Roadmap</li>
+        </ul>
+      
+    </div>
+    </header>)
+ }
